ad1c693fd0c6b68e9cf4ac9ab6c89ec59984fc85
ankud1nov <ankud1nov@yandex.ru>
2021-05-16 14:43:23 +0400

    Версия 1.0

---
+++ ConsoleSender/Message.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSender
+{
+    class Message
+    {
+        int number;
+        DateTime timeSend;
+        string comment;
+        string text;
+        string hashSumm;
+        public Message()
+        {
+            number = 1;
+            comment = "";
+            SetParams();
+        }
+        public Message(string comment)
+        {
+            number = 1;
+            this.comment = comment;
+            SetParams();
+        }
+        public void Next()
+        {
+            number += 1;
+            SetParams();
+        }
+        public void Next(string comment)
+        {
+            number += 1;
+            this.comment = comment;
+            SetParams();
+        }
+        public string GetMessage()
+        {
+            return text;
+        }
+        void SetParams()
+        {
+            timeSend = DateTime.Now;
+            text = $"{number}. \"{timeSend}\" Комментарий: {comment}";
+            hashSumm = $"{number}{timeSend}{comment}";
+        }
+    }
+}
--- ConsoleSender/Program.cs
+++ ConsoleSender/Program.cs
@@ -1,15 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Text;
+using RabbitMQ.Client;
 using System.Threading.Tasks;
 
 namespace ConsoleSender
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            Message message = new Message("Comment");
+            var factory = new ConnectionFactory() { HostName = "localhost"};
+            while (true)
+            {
+                try
+                {
+                    using (var connection = factory.CreateConnection())
+                        using (var channel = connection.CreateModel())//Инициализация подключения
+                    {
+                        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);//Описание очереди
+
+                        var text = "";
+                        while (text != "0")
+                        {
+                            var messageText = message.GetMessage();
+                            var body = Encoding.UTF8.GetBytes(messageText);
+                            try
+                            {
+                                channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
+                                Console.WriteLine($" [x] Sent {messageText}");
+                                message.Next(GetMessage(args));
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($" [x] Sent {ex.Message}");
+                                //throw;
+                            }
+                            Thread.Sleep(1000);
+                        }
+
+                    }
+                }
+                catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException ex)
+                {
+                    Console.WriteLine($" [x] Error {{{ex.Message}}}");
+                }
+            }
+            return 0;
         }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "No comments");
+        }
+
     }
 }
